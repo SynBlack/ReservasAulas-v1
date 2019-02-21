92b366c7509de05f028402c210dc0f73b938b627
Darkcrow <Darkcrow@192.168.1.102>
2019-02-21 13:18:06 +0100

    Modificación de la clase Aulas.

--- src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Aulas.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Aulas.java
@@ -1,9 +1,6 @@
-/**
- * 
- */
 package org.iesalandalus.programacion.reservasaulas.modelo.dao;
 
-import java.io.NotActiveException;
+import java.util.ArrayList;
 
 import javax.naming.OperationNotSupportedException;
 
@@ -14,120 +11,75 @@ import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Aula;
  *
  */
 public class Aulas {
-	private static final int MAX_AULAS = 50;
-	private int numAulas;
-	private Aula[] conjuntoAulas;
-	
+	private ArrayList<Aula> conjuntoAulas;
+
 	public Aulas() {
-		this.numAulas = 0;
-		this.conjuntoAulas = new Aula[MAX_AULAS];
+		this.conjuntoAulas = new ArrayList<>();
 	}
-	
+
 	public Aulas(Aulas aulas) {
 		this.setAulas(aulas);
 	}
-	
+
 	private void setAulas(Aulas aulas) {
 		if (aulas == null) {
 			throw new IllegalArgumentException("No se pueden copiar aulas nulas.");
 		}
 		this.conjuntoAulas = this.copiaProfundaAulas(aulas.conjuntoAulas);
-		this.numAulas = aulas.numAulas;
 	}
-	
-	private Aula[] copiaProfundaAulas(Aula[] conjuntoAulas) {
-		Aula[] conjuntoAulasSalida = new Aula[conjuntoAulas.length];
-		for (int i = 0; i < conjuntoAulas.length && conjuntoAulas[i] != null; i++) {
-			conjuntoAulasSalida[i] = new Aula(conjuntoAulas[i]);
+
+	private ArrayList<Aula> copiaProfundaAulas(ArrayList<Aula> conjuntoAulas) {
+		ArrayList<Aula> conjuntoAulasSalida = new ArrayList<>();
+		for (Aula aula : conjuntoAulas) {
+			conjuntoAulasSalida.add(new Aula(aula));
 		}
 		return conjuntoAulasSalida;
-		
+
 	}
-	
-	public Aula[] getAulas() {
+
+	public ArrayList<Aula> getAulas() {
 		return this.copiaProfundaAulas(this.conjuntoAulas);
 	}
 
 	public int getNumAulas() {
-		return this.numAulas;
+		return this.conjuntoAulas.size();
 	}
-	
+
 	public void insertar(Aula aula) throws OperationNotSupportedException {
 		if (aula == null) {
 			throw new IllegalArgumentException("No se puede insertar un aula nula.");
 		}
-		int indice = buscarIndiceAula(aula);
-		if (indiceNoSuperaTamano(indice) == false) {
-			this.conjuntoAulas[indice] = new Aula(aula);
-			this.numAulas++;
+		if (this.conjuntoAulas.contains(aula)) {
+			throw new OperationNotSupportedException("El aula ya existe.");
 		} else {
-			if (indiceNoSuperaCapacidad(indice)) {
-				throw new OperationNotSupportedException("El aula ya existe.");
-			} else {
-				throw new OperationNotSupportedException("No se aceptan más aulas.");
-			}
+			this.conjuntoAulas.add(aula);
 		}
-	}
-	
-	private int buscarIndiceAula(Aula aula) {
-		int indice = 0;
-		boolean localizado = false;
-		while (indiceNoSuperaTamano(indice) && localizado == false) {
-			if (this.conjuntoAulas[indice].equals(aula)) {
-				localizado = true;
-			} else {
-				indice++;
-			}
-		}
-		return indice;
-	}
-	
-	private boolean indiceNoSuperaTamano(int indice) {
-		return indice < this.numAulas;
 	}
 
-	private boolean indiceNoSuperaCapacidad(int indice) {
-		return indice < this.MAX_AULAS;
-	}
-	
 	public Aula buscar(Aula aula) {
-		Aula localizada = null;
-		int indice = buscarIndiceAula(aula);
-		if (indiceNoSuperaTamano(indice)) {
-			localizada = new Aula(this.conjuntoAulas[indice]);
+		Aula aulaSalida = null;
+		if (this.conjuntoAulas.contains(aula)) {
+			aulaSalida = new Aula(aula);
 		}
-		return localizada;
+		return aulaSalida;
 	}
-	
+
 	public void borrar(Aula aula) throws OperationNotSupportedException {
 		if (aula == null) {
 			throw new IllegalArgumentException("No se puede borrar un aula nula.");
 		}
-		int indice = buscarIndiceAula(aula);
-		if (indiceNoSuperaTamano(indice)) {
-			this.desplazarUnaPosicionHaciaIzquierda(indice);
+		if (this.conjuntoAulas.contains(aula)) {
+			this.conjuntoAulas.remove(aula);
 		} else {
 			throw new OperationNotSupportedException("El aula a borrar no existe.");
 		}
 	}
 
-	private void desplazarUnaPosicionHaciaIzquierda(int indice) {
-		for (int i = indice; i < this.numAulas - 1; i++) {
-			this.conjuntoAulas[i] = this.conjuntoAulas[i + 1];
+	public ArrayList<String> representar() {
+		ArrayList<String> cadenaSalida = new ArrayList<>();
+		for (Aula aula : this.conjuntoAulas) {
+			cadenaSalida.add(aula.toString());
 		}
-		this.conjuntoAulas[this.numAulas] = null;
-		this.numAulas--;
-	}
-
-	public String[] representar() {
-		String[] aulasCadena = null;
-		if (this.numAulas > 0) {
-			aulasCadena = new String[this.numAulas];
-			for (int i = 0; indiceNoSuperaTamano(i); i++) {
-				aulasCadena[i] = this.conjuntoAulas[i].toString();
-				System.out.println(i+1 + ".- " + aulasCadena[i]);
-			}
-		}
-		return aulasCadena;
+		return cadenaSalida;
 	}
 }
