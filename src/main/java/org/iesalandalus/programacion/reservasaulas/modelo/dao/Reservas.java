c92ea0496db893d9d72c5bc5da060bf7ca076218
Darkcrow <Darkcrow@192.168.1.102>
2019-02-21 13:34:39 +0100

    Modificación en la clase Reservas.

--- src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Reservas.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dao/Reservas.java
@@ -5,6 +5,7 @@ import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Aula;
 import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Permanencia;
 import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Profesor;
 import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Reserva;
+import java.util.ArrayList;
 
 /**
  * @author Emanuel Martínez Lonardi
@@ -12,13 +13,10 @@ import org.iesalandalus.programacion.reservasaulas.modelo.dominio.Reserva;
  */
 
 public class Reservas {
-	private static final int MAX_RESERVAS = 50;
-	private int numReservas;
-	private Reserva[] reservasRealizadas;
+	private ArrayList<Reserva> reservasRealizadas;
 
 	public Reservas() {
-		this.numReservas = 0;
-		this.reservasRealizadas = new Reserva[this.MAX_RESERVAS];
+		this.reservasRealizadas = new ArrayList<>();
 	}
 
 	public Reservas(Reservas reservas) {
@@ -30,68 +28,39 @@ public class Reservas {
 			throw new IllegalArgumentException("No se pueden copiar reservas nulas.");
 		}
 		this.reservasRealizadas = this.copiaProfundaReservas(reservas.reservasRealizadas);
-		this.numReservas = reservas.numReservas;
 	}
 
-	private Reserva[] copiaProfundaReservas(Reserva[] reservas) {
-		Reserva[] reservasCopia = new Reserva[reservas.length];
-		for (int i = 0; i < reservas.length && reservas[i] != null; i++) {
-			reservasCopia[i] = new Reserva(reservas[i]);
+	private ArrayList<Reserva> copiaProfundaReservas(ArrayList<Reserva> reservas) {
+		ArrayList<Reserva> reservasSalida = new ArrayList<>();
+		for (Reserva reserva : reservas) {
+			reservasSalida.add(new Reserva(reserva));
 		}
-		return reservasCopia;
+		return reservasSalida;
 	}
 
-	public Reserva[] getReservas() {
+	public ArrayList<Reserva> getReservas() {
 		return this.copiaProfundaReservas(this.reservasRealizadas);
 	}
 
 	public int getNumReservas() {
-		return this.numReservas;
+		return this.reservasRealizadas.size();
 	}
 
 	public void insertar(Reserva reserva) throws OperationNotSupportedException {
 		if (reserva == null) {
 			throw new IllegalArgumentException("No se puede realizar una reserva nula.");
 		}
-		int indice = this.buscarIndiceReserva(reserva);
-		if (this.indiceNoSuperaTamano(indice) == false) {
-			this.reservasRealizadas[this.numReservas] = new Reserva(reserva);
-			this.numReservas++;
+		if (this.reservasRealizadas.contains(reserva)) {
+			throw new OperationNotSupportedException("La reserva ya existe.");
 		} else {
-			if (this.indiceNoSuperaCapacidad(indice)) {
-				throw new OperationNotSupportedException("La reserva ya existe.");
-			} else {
-				throw new OperationNotSupportedException("No se permiten más reservas.");
-			}
+			this.reservasRealizadas.add(reserva);
 		}
-	}
-
-	private int buscarIndiceReserva(Reserva reserva) {
-		int indice = 0;
-		boolean encontrada = false;
-		while (indiceNoSuperaTamano(indice) && encontrada == false) {
-			if (this.reservasRealizadas[indice].equals(reserva)) {
-				encontrada = true;
-			} else {
-				indice++;
-			}
-		}
-		return indice;
-	}
-
-	private boolean indiceNoSuperaTamano(int indice) {
-		return indice < this.numReservas;
-	}
-
-	private boolean indiceNoSuperaCapacidad(int indice) {
-		return indice < this.MAX_RESERVAS;
 	}
 
 	public Reserva buscar(Reserva reserva) {
 		Reserva reservaSalida = null;
-		int indice = this.buscarIndiceReserva(reserva);
-		if (this.indiceNoSuperaTamano(indice) == true) {
-			reservaSalida = new Reserva(this.reservasRealizadas[indice]);
+		if (this.reservasRealizadas.contains(reserva)) {
+			reservaSalida = new Reserva(reserva);
 		}
 		return reservaSalida;
 	}
@@ -100,93 +69,63 @@ public class Reservas {
 		if (reserva == null) {
 			throw new IllegalArgumentException("No se puede anular una reserva nula.");
 		}
-		int indice = this.buscarIndiceReserva(reserva);
-		if (this.indiceNoSuperaTamano(indice)) {
-			this.desplazarUnaPosicionHaciaIzquierda(indice);
+		if (this.reservasRealizadas.contains(reserva)) {
+			this.reservasRealizadas.remove(reserva);
 		} else {
 			throw new OperationNotSupportedException("La reserva a anular no existe.");
 		}
 	}
 
-	private void desplazarUnaPosicionHaciaIzquierda(int posicion) {
-		for (int i = posicion; i < this.numReservas - 1; i++) {
-			this.reservasRealizadas[i] = this.reservasRealizadas[i + 1];
+	public ArrayList<String> representar() {
+		ArrayList<String> cadenaSalida = new ArrayList<>();
+		for (Reserva reserva : this.reservasRealizadas) {
+			cadenaSalida.add(reserva.toString());
 		}
-		this.reservasRealizadas[this.numReservas] = null;
-		this.numReservas--;
+		return cadenaSalida;
 	}
 
-	public String[] representar() {
-		String[] reservasCadena = null;
-		if(this.numReservas > 0) {
-			reservasCadena = new String[this.numReservas];
-			for (int i = 0; this.indiceNoSuperaTamano(i); i++) {
-				reservasCadena[i] = this.reservasRealizadas[i].toString();
-				System.out.println(i+1 + ".- " + reservasCadena[i]);
-			}
-		}
-		return reservasCadena;
-	}
-
-	public Reserva[] getReservasProfesor(Profesor profesor) {
-		Reserva[] reservasProfesor = new Reserva[this.MAX_RESERVAS];
-		int indice = 0;
-		if(profesor == null) {
-			throw new IllegalArgumentException("");
-		}
-		for(int i = 0; i < this.numReservas; i++) {
-			if(this.reservasRealizadas[i].getProfesor().equals(profesor)) {
-				reservasProfesor[indice] = new Reserva(this.reservasRealizadas[i]);
-				indice++;
+	public ArrayList<Reserva> getReservasProfesor(Profesor profesor) {
+		ArrayList<Reserva> reservasProfesor = new ArrayList<>();
+		for (Reserva reserva : this.reservasRealizadas) {
+			if (reserva.getProfesor().equals(profesor)) {
+				reservasProfesor.add(new Reserva(reserva));
 			}
 		}
 		return reservasProfesor;
 	}
 
-	public Reserva[] getReservasAula(Aula aula) {
-		Reserva[] reservasAulas = new Reserva[this.MAX_RESERVAS];
-		int indice = 0;
-		if(aula == null) {
-			throw new IllegalArgumentException("");
-		}
-		for(int i = 0; i < this.numReservas; i++) {
-			if(this.reservasRealizadas[i].getAula().equals(aula)) {
-				reservasAulas[indice] = new Reserva(this.reservasRealizadas[i]);
-				indice++;
+	public ArrayList<Reserva> getReservasAula(Aula aula) {
+		ArrayList<Reserva> reservasAulas = new ArrayList<>();
+		for (Reserva reserva : this.reservasRealizadas) {
+			if (reserva.getAula().equals(aula)) {
+				reservasAulas.add(new Reserva(reserva));
 			}
 		}
 		return reservasAulas;
 	}
 
-	public Reserva[] getReservasPermanencia(Permanencia permanencia) {
-		Reserva[] reservasPermanencia = new Reserva[this.MAX_RESERVAS];
-		int indice = 0;
-		if(permanencia == null) {
-			throw new IllegalArgumentException("");
-		}
-		for(int i = 0; i < this.numReservas; i++) {
-			if(this.reservasRealizadas[i].getPermanencia().equals(permanencia)) {
-				reservasPermanencia[indice] = new Reserva(this.reservasRealizadas[i]);
-				indice++;
+	public ArrayList<Reserva> getReservasPermanencia(Permanencia permanencia) {
+		ArrayList<Reserva> reservasPermanencia = new ArrayList<>();
+		for (Reserva reserva : this.reservasRealizadas) {
+			if (reserva.getPermanencia().equals(permanencia)) {
+				reservasPermanencia.add(new Reserva(reserva));
 			}
 		}
 		return reservasPermanencia;
 	}
 
 	public boolean consultarDisponibilidad(Aula aula, Permanencia permanencia) {
-		if(aula == null) {
+		if (aula == null) {
 			throw new IllegalArgumentException("No se puede consultar la disponibilidad de un aula nula.");
 		}
-		if(permanencia == null) {
+		if (permanencia == null) {
 			throw new IllegalArgumentException("No se puede consultar la disponibilidad de una permanencia nula.");
 		}
-		boolean disponible = true;
-		for(int i = 0; i < this.numReservas; i++) {
-			if(this.reservasRealizadas[i].getAula().equals(aula) &
-					this.reservasRealizadas[i].getPermanencia().equals(permanencia)) {
-				disponible = false;
+		for (Reserva reserva : this.reservasRealizadas) {
+			if (reserva.getAula().equals(aula) && reserva.getPermanencia().equals(permanencia)) {
+				return false;
 			}
 		}
-		return disponible;
+		return true;
 	}
 }
